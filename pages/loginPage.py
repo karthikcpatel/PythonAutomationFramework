9c026407d962e69c5d6c8bc178bce2c91d3a7055
Kartik Patel <kartik.patel@vertexinc.com>
2023-01-30 07:43:32 +0530

    30th Jan, 2023.

--- pages/loginPage.py
+++ pages/loginPage.py
@@ -1,3 +1,5 @@
+from selenium.webdriver.common.by import By
+
 class LoginPage():
 
     def __init__(self, driver):
@@ -11,16 +13,18 @@ class LoginPage():
     def enter_username(self, username):
         """This function will enter username is username textbox.
         """
-        self.driver.find_element_by_name(self.username_textbox_name).clear()
-        self.driver.find_element_by_name(self.username_textbox_name).send_keys(username)
+        self.driver.find_element(By.NAME,self.username_textbox_name).clear
+        self.driver.find_element(By.NAME,self.username_textbox_name).send_keys(username)
+        #self.driver.find_element_by_name(self.username_textbox_name).clear()
+        #self.driver.find_element_by_name(self.username_textbox_name).send_keys(username)
 
     def enter_password(self, password):
         """This function will enter password is password textbox.
         """
-        self.driver.find_element_by_name(self.password_textbox_name).clear()
-        self.driver.find_element_by_name(self.password_textbox_name).send_keys(password)
+        self.driver.find_element(By.NAME,self.password_textbox_name).clear
+        self.driver.find_element(By.NAME,self.password_textbox_name).send_keys(password)
 
     def click_login(self):
         """This function will click on login button.
         """
-        self.driver.find_element_by_xpath(self.login_button_xpath).click()
+        self.driver.find_element(By.XPATH, self.login_button_xpath).click()
--- update_packages.bat
+++ update_packages.bat
@@ -1,7 +1,15 @@
-pip install selenium
+pip install selenium==4.3.0
 pip install moment
-pip install webdriver-manager
-pip install pytest
-pip install pytest-bdd
-pip install allure-pytest==2.9.44
+pip install webdriver-manager==3.8.0
+pip install pytest==6.2.5
+pip install pytest-bdd==5.0.0
+pip install xmldiff==2.4
+pip install requests==2.28.0
+pip install pywinauto==0.6.8
+pip install lxml==4.8.0
+pip install jsonpath==0.82
+pip install allure-pytest==2.9.45
+pip install allure-python-commons==2.9.45
+pip install pytest-html==3.1.1
+pip install pandas==1.4.4
 pause
