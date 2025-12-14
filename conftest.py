3497dbf24adf88877426a5b61f28b9dffa70345c
Kartik Patel <karthikcpatel@gmail.com>
2025-12-14 20:21:19 +0530

    14th Dec, 2025.

--- conftest.py
+++ conftest.py
@@ -6,20 +6,20 @@ def pytest_addoption(parser):
     parser.addoption("--browser", action="store", default="chrome", help="Type in browser name e.g. chrome OR firefox")
 
 @pytest.fixture(scope="class")
+
 def test_setup(request):
     from selenium import webdriver
 
     browser = request.config.getoption("--browser")
 
     if browser == 'chrome':
-        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+        driver = webdriver.Chrome()
     elif browser == 'firefox':
-        driver = webdriver.Firefox(executable_path="C:/Users/kartikp/PycharmProjects/PythonAutomationFramework/drivers/geckodriver.exe")
+        driver = webdriver.Firefox()
 
     driver.implicitly_wait(5)
     driver.maximize_window()
+
     request.cls.driver = driver
     yield
-    #driver.close()
     driver.quit()
-    print("Test Completed")
--- pages/loginPage.py
+++ pages/loginPage.py
@@ -1,30 +1,23 @@
 from selenium.webdriver.common.by import By
 
-class LoginPage():
+class LoginPage:
 
     def __init__(self, driver):
         self.driver = driver
 
         self.username_textbox_name = "username"
-        #self.username_textbox_placeholder = "Username"
         self.password_textbox_name = "password"
         self.login_button_xpath = "//button[@type='submit']"
 
     def enter_username(self, username):
-        """This function will enter username is username textbox.
-        """
-        self.driver.find_element(By.NAME,self.username_textbox_name).clear
-        self.driver.find_element(By.NAME,self.username_textbox_name).send_keys(username)
-        #self.driver.find_element_by_name(self.username_textbox_name).clear()
-        #self.driver.find_element_by_name(self.username_textbox_name).send_keys(username)
+        element = self.driver.find_element(By.NAME, self.username_textbox_name)
+        element.clear()
+        element.send_keys(username)
 
     def enter_password(self, password):
-        """This function will enter password is password textbox.
-        """
-        self.driver.find_element(By.NAME,self.password_textbox_name).clear
-        self.driver.find_element(By.NAME,self.password_textbox_name).send_keys(password)
+        element = self.driver.find_element(By.NAME, self.password_textbox_name)
+        element.clear()
+        element.send_keys(password)
 
     def click_login(self):
-        """This function will click on login button.
-        """
         self.driver.find_element(By.XPATH, self.login_button_xpath).click()
